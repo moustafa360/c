8fb1d959bf98dc3f6db8ee0afaf6dc495cc07fce
moustafa360 <44622844+moustafa360@users.noreply.github.com>
2020-10-31 17:41:44 +0200

    Update palindrome.c

--- algorithms/strings/palindrome.c
+++ algorithms/strings/palindrome.c
@@ -9,17 +9,19 @@
 int main()
 {
   char a[100], b[100];
+   int L;
 
   printf("Enter a string to check if it's a palindrome\n");
   gets(a);
 
   strcpy(b, a);  // Copying input string
   strrev(b);  // Reversing the string
+   L=strlen(a); 
 
   if (strcmp(a, b) == 0)  // Comparing input string with the reverse string
-    printf("The string is a palindrome.\n");
+    printf("The string (%s) is a palindrome.\n",a);
   else
-    printf("The string isn't a palindrome.\n");
+    printf("The string (%s) is not a palindrome.\n",a);
 
   return 0;
 }
